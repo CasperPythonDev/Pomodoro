ccacaaa2813a7110837e279cfdda002848d26900
Casper Olsen <casper90.n.olsen@gmail.com>
2023-08-30 12:28:57 +0200

    Finish base UI design

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import tkinter as tk
+from tkinter import Label, Button
 
 # ---------------------------- CONSTANTS ------------------------------- #
 PINK = "#e2979c"
@@ -31,9 +32,22 @@ window.minsize(width=WIDTH, height=HEIGHT)
 canvas = tk.Canvas(width=200, height=224, bg=YELLOW, highlightthickness=0)
 background_image = tk.PhotoImage(file="images/tomato.png")
 canvas.create_image(100, 112, image=background_image)
-canvas.create_text(100, 130, text="00:00", fill="white", font=(FONT_NAME, 35, "bold"))
-canvas.pack()
+canvas.create_text(100, 130, text="00:00", fill="white", font=(FONT_NAME, 35))
+canvas.grid(column=1, row=1)
 
+# Labels
+title = Label(text="Timer", fg=GREEN, bg=YELLOW, font=(FONT_NAME, 40, "bold"))
+title.grid(column=1, row=0)
+
+check_marks = Label(text="✔", bg=YELLOW, fg=GREEN, font=(FONT_NAME, 12))
+check_marks.grid(column=1, row=3)
+
+# Buttons
+start_button = Button(text="Start", highlightthickness=0)
+start_button.grid(column=0, row=2)
+
+reset_button = Button(text="Reset", highlightthickness=0)
+reset_button.grid(column=2, row=2)
 
 #
 app.mainloop()
