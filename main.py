3d2fc38e66f02300b7f34a0980b9234868e5fff3
Casper Olsen <casper90.n.olsen@gmail.com>
2023-09-01 10:27:14 +0200

    Add countdown logic and minor GUI changes.

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import tkinter as tk
 from tkinter import Label, Button
+from math import floor
 
 # ---------------------------- CONSTANTS ------------------------------- #
 PINK = "#e2979c"
@@ -10,12 +11,57 @@ FONT_NAME = "Courier"
 WORK_MIN = 25
 SHORT_BREAK_MIN = 5
 LONG_BREAK_MIN = 20
+reps = 0  # not a constant, consider refactor
 
 # ---------------------------- TIMER RESET ------------------------------- #
 
+
 # ---------------------------- TIMER MECHANISM ------------------------------- #
+def start_timer():  # Alternative to lambda
+    global reps
+    reps += 1
+    # work_timer = WORK_MIN * 60
+    # short_b_timer = SHORT_BREAK_MIN * 60
+    # long_b_timer = LONG_BREAK_MIN * 60
+    work_timer = 5  # Test
+    short_b_timer = 2  # Test
+    long_b_timer = 10  # Test
+
+    if reps % 8 == 0:
+        count_down(long_b_timer)
+        print(f"Long break: {reps}")
+        title.config(text="Break", fg=RED)
+    elif reps % 4 == 0:
+        print(f"Short break: {reps}")
+        count_down(short_b_timer)
+        title.config(text="Break", fg=PINK)
+    else:
+        count_down(work_timer)
+        print(f"Work flow: {reps}")
+        title.config(text="Work", fg=GREEN)
+
 
 # ---------------------------- COUNTDOWN MECHANISM ------------------------------- #
+def count_down(count):
+    min_remaining = floor(count / 60)
+    sec_remaining = count % 60
+
+    if sec_remaining < 10:
+        sec_block = f"0{sec_remaining}"
+    else:
+        sec_block = sec_remaining
+
+    min_block = min_remaining  # Consider same logic as above for min
+
+    timer = f"{min_block}:{sec_block}"
+    canvas.itemconfig(canvas_timer, text=timer)
+
+    if count > -1:
+        # print(count)  # For testing
+        app.after(1000, count_down, count - 1)
+    else:
+        start_timer()
+
 
 # ---------------------------- UI SETUP ------------------------------- #
 app = tk.Tk()
@@ -32,7 +78,9 @@ window.minsize(width=WIDTH, height=HEIGHT)
 canvas = tk.Canvas(width=200, height=224, bg=YELLOW, highlightthickness=0)
 background_image = tk.PhotoImage(file="images/tomato.png")
 canvas.create_image(100, 112, image=background_image)
-canvas.create_text(100, 130, text="00:00", fill="white", font=(FONT_NAME, 35))
+canvas_timer = canvas.create_text(
+    100, 130, text="00:00", fill="white", font=(FONT_NAME, 35, "bold")
+)
 canvas.grid(column=1, row=1)
 
 # Labels
@@ -43,11 +91,14 @@ check_marks = Label(text="✔", bg=YELLOW, fg=GREEN, font=(FONT_NAME, 12))
 check_marks.grid(column=1, row=3)
 
 # Buttons
-start_button = Button(text="Start", highlightthickness=0)
+start_button = Button(text="Start", highlightthickness=0, command=start_timer)
+# lambda function allows us to pass argument with function in TKinter framework. # command=lambda: count_down(5)
 start_button.grid(column=0, row=2)
 
 reset_button = Button(text="Reset", highlightthickness=0)
 reset_button.grid(column=2, row=2)
+# - function calls
 
-#
+
+# - mainloop
 app.mainloop()
